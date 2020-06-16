0e67a1fb59ee8959e200d4e2bbd215f80d3236b2
ivannz <ivannz@yandex.ru>
2020-06-16 22:14:10 +0300

    package skeleton

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='MarkovDWP',
+    version='0.1',
+    description='''Backend for experiments with Deep Weight Prior''',
+    license='MIT License',
+    packages=[
+        'markovdwp',
+    ],
+)
