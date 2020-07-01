79f6cc8c57a25af02f98ef99904b2d26b84f4335
ivannz <ivannz@yandex.ru>
2020-07-01 12:55:14 +0300

    richer augmentation options for cifar

--- markovdwp/source/cifar/dataset.py
+++ markovdwp/source/cifar/dataset.py
@@ -2,23 +2,37 @@ from torchvision import datasets, transforms
 from ..utils.dataset import stratified_split
 
 
+def get_transform(augmentation):
+    if isinstance(augmentation, bool):
+        augmentation = 'full' if augmentation else 'normalize'
+
+    assert augmentation in ('none', 'crop-flip', 'normalize', 'full')
+
+    transform, normalize = [], []
+    if augmentation in ('full', 'crop-flip'):
+        transform = [
+            transforms.RandomCrop(32, padding_mode='reflect', padding=4),
+            transforms.RandomHorizontalFlip()
+        ]
+
+    if augmentation in ('full', 'normalize'):
+        normalize = [
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2023, 0.1994, 0.2010))
+        ]
+
+    return transforms.Compose([
+        *transform, transforms.ToTensor(), *normalize
+    ])
+
+
 class CIFAR10_Train:
     """Train sample from the CIFAR10 dataset."""
-    def __new__(cls, root, augmentation=False,
+    def __new__(cls, root, augmentation='normalize',
                 train_size=None, random_state=None):
-        transform = []
-        if augmentation:
-            transform = [
-                transforms.RandomCrop(32, padding_mode='reflect', padding=4),
-                transforms.RandomHorizontalFlip()
-            ]
-
-        dataset = datasets.CIFAR10(root, transform=transforms.Compose([
-            *transform,
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465),
-                                 (0.2023, 0.1994, 0.2010)),
-        ]), target_transform=None, train=True, download=True)
+        transform = get_transform(augmentation)
+        dataset = datasets.CIFAR10(root, transform=transform, train=True,
+                                   target_transform=None, download=True)
 
         train, _ = stratified_split(dataset, train_size=train_size,
                                     random_state=random_state)
@@ -27,31 +41,19 @@ class CIFAR10_Train:
 
 class CIFAR10_Test:
     """Test sample from the CIFAR10 dataset."""
-    def __new__(cls, root):
-        return datasets.CIFAR10(root, transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465),
-                                 (0.2023, 0.1994, 0.2010)),
-        ]), target_transform=None, train=False, download=True)
+    def __new__(cls, root, augmentation='normalize'):
+        transform = get_transform(augmentation)
+        return datasets.CIFAR10(root, transform=transform, train=False,
+                                target_transform=None, download=True)
 
 
 class CIFAR100_Train:
-    """Train sample from the CIFAR10 dataset."""
-    def __new__(cls, root, augmentation=False,
+    """Train sample from the CIFAR100 dataset."""
+    def __new__(cls, root, augmentation='normalize',
                 train_size=None, random_state=None):
-        transform = []
-        if augmentation:
-            transform = [
-                transforms.RandomCrop(32, padding_mode='reflect', padding=4),
-                transforms.RandomHorizontalFlip()
-            ]
-
-        dataset = datasets.CIFAR100(root, transform=transforms.Compose([
-            *transform,
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465),
-                                 (0.2023, 0.1994, 0.2010)),
-        ]), target_transform=None, train=True, download=True)
+        transform = get_transform(augmentation)
+        dataset = datasets.CIFAR100(root, transform=transform, train=True,
+                                    target_transform=None, download=True)
 
         train, _ = stratified_split(dataset, train_size=train_size,
                                     random_state=random_state)
@@ -60,9 +62,7 @@ class CIFAR100_Train:
 
 class CIFAR100_Test:
     """Test sample from the CIFAR100 dataset."""
-    def __new__(cls, root):
-        return datasets.CIFAR100(root, transform=transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465),
-                                 (0.2023, 0.1994, 0.2010)),
-        ]), target_transform=None, train=False, download=True)
+    def __new__(cls, root, augmentation='normalize'):
+        transform = get_transform(augmentation)
+        return datasets.CIFAR100(root, transform=transform, train=False,
+                                 target_transform=None, download=True)
