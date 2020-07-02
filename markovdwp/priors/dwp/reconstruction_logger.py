1f878453bc02eaf14c8804440c31d06f6cfaa07e
ivannz <ivannz@yandex.ru>
2020-07-02 10:48:01 +0300

    device placement mixup and argument rename

--- markovdwp/priors/dwp/reconstruction_logger.py
+++ markovdwp/priors/dwp/reconstruction_logger.py
@@ -27,30 +27,26 @@ def scatter(data, **kwargs):
     return fig
 
 
-def forward(pl_module, x, sample=True):
+def forward(vae, x, sample=True):
     # make sure to re-enable train mode and grads!
-    assert isinstance(pl_module, pl.LightningModule)
-
-    pl_module.eval()
+    vae.eval()
     with torch.no_grad():
         # E_{z ~ q(z|x_0)} \log p(x_0|z)
-        q = pl_module.encoder(x.to(pl_module.device))
-        p = pl_module.decoder(q.sample() if sample else q.mean)
-    pl_module.train()
+        q = vae.encoder(x)
+        p = vae.decoder(q.sample() if sample else q.mean)
+    vae.train()
 
     return p, q
 
 
-def reverse(pl_module, z, sample=True):
+def reverse(vae, z, sample=True):
     # make sure to re-enable train mode and grads!
-    assert isinstance(pl_module, pl.LightningModule)
-
-    pl_module.eval()
+    vae.eval()
     with torch.no_grad():
         # E_{x ~ p(x|z_0)} \log q(z_0|x)
-        p = pl_module.decoder(z.to(pl_module.device))
-        q = pl_module.encoder(p.sample() if sample else p.mean)
-    pl_module.train()
+        p = vae.decoder(z)
+        q = vae.encoder(p.sample() if sample else p.mean)
+    vae.train()
 
     return p, q
 
@@ -76,26 +72,29 @@ class SliceReconstructionLogger(Callback):
 
     def on_epoch_end(self, trainer, pl_module):
         # A training epoch has just finished
-        p, q = forward(pl_module, self.ref_x, sample=self.sample)
+        ref_x = self.ref_x.to(pl_module.device)
+
+        p, q = forward(pl_module, ref_x, sample=self.sample)
         rec = p.sample() if self.sample else p.mean
 
         # V_{x ~ data} E_{z ~ q(z|x) z}
         activity = q.mean.squeeze().std(0).flatten().cpu()
         output = {
             'task/rec': plot_slices(rec[:, 0], **self.imshow),
-            'diag/ll_x': p.log_prob(self.ref_x).mean().cpu(),
+            'diag/ll_x': p.log_prob(ref_x).mean().cpu(),
             **{f'diag/A_u{i}': a for i, a in enumerate(activity)},
         }
         if self.scatter:
             output['diag/lat_z'] = scatter(q.mean.squeeze())
 
         if self.ref_z is not None:
-            r, e = reverse(pl_module, self.ref_z, sample=self.sample)
+            ref_z = self.ref_z.to(pl_module.device)
+            r, e = reverse(pl_module, ref_z, sample=self.sample)
             gen = r.sample() if self.sample else r.mean
 
             output.update({
                 'task/gen': plot_slices(gen[:, 0], **self.imshow),
-                'diag/ll_z': e.log_prob(self.ref_z).mean().cpu(),
+                'diag/ll_z': e.log_prob(ref_z).mean().cpu(),
             })
 
         # commit with the next call to pl's logger
