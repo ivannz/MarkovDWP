2650270439c9025335b0a9b8b5849603e822c54a
ivannz <ivannz@yandex.ru>
2020-07-12 15:47:53 +0300

    update to new dicttols interface

--- markovdwp/source/utils/common.py
+++ markovdwp/source/utils/common.py
@@ -2,13 +2,13 @@ from ...utils.dicttools import flatten, aggregate, propagate
 
 
 def prepare_log(details, level=5, delim='/'):
-    prepared = aggregate(flatten(details, delim='.'), level=level, sep='.')
+    prepared = aggregate(flatten(details, delim='.'), level=level, delim='.')
     return {k.replace('.', delim): v for k, v in prepared.items()}
 
 
 def weighted_sum(terms, **coef):
     # 1. compute the final loss
-    C = dict(propagate({'': 1.0, **coef}, terms))
+    C = dict(propagate({'': 1.0, **coef}, terms, delim='.'))
     value = sum(v * C[k] for k, v in terms.items())
 
     # 2. return differentiable loss and its components as floats
