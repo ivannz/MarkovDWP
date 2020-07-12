19aa3870c995bd1fcdf4efbf385b25b231ba4894
ivannz <ivannz@yandex.ru>
2020-07-12 15:39:31 +0300

    custom delimiter in propagate and prefix-adder

--- markovdwp/utils/dicttools.py
+++ markovdwp/utils/dicttools.py
@@ -2,7 +2,7 @@ import copy
 from collections import defaultdict
 
 
-def propagate(lookup, G, prefix="", value=None):
+def propagate(lookup, G, prefix='', value=None, delim='.'):
     """Assign values propagating if necessary.
 
     Details
@@ -12,7 +12,8 @@ def propagate(lookup, G, prefix="", value=None):
     """
     # '' is a the parent of all nodes (except itself)
     if '' in G:
-        yield from propagate(lookup, set(n for n in G if n), prefix, value)
+        yield from propagate(lookup, set(n for n in G if n),
+                             prefix, value, delim)
         return
 
     # lookup (or inherit) the parent's value
@@ -20,22 +21,27 @@ def propagate(lookup, G, prefix="", value=None):
     yield prefix, value
 
     # collect children of the current prefix (aka `parent`)
-    children, prefix = {}, prefix + ('.' if prefix else '')
+    children, prefix = {}, prefix + (delim if prefix else '')
     for node in G:
-        name, dot, child = node.partition('.')
+        name, dot, child = node.partition(delim)
         children.setdefault(prefix + name, set())
         if child:
             children[prefix + name].add(child)
 
     # propagate this parent's value to its children
     for prefix, G in children.items():
-        yield from propagate(lookup, G, prefix, value)
+        yield from propagate(lookup, G, prefix, value, delim)
 
 
-def aggregate(dictionary, level=0, sep='.'):
+def add_prefix(dictionary, prefix='', delim='.'):
+    prefix = prefix + (delim if prefix else '')
+    return {prefix + k: v for k, v in dictionary.items()}
+
+
+def aggregate(dictionary, level=0, delim='.'):
     out, groups = {}, defaultdict(dict)
     for k, v in dictionary.items():
-        key, _, child = k.partition(sep)
+        key, _, child = k.partition(delim)
         if not child:
             out[key] = v
         else:
@@ -45,8 +51,8 @@ def aggregate(dictionary, level=0, sep='.'):
         if level <= 0:
             out[name] = sum(group.values())
         else:
-            group = aggregate(group, level - 1)
-            out.update((name + sep + k, val) for k, val in group.items())
+            group = aggregate(group, level - 1, delim=delim)
+            out.update((name + delim + k, val) for k, val in group.items())
     return out
 
 
