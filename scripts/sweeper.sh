49b19667ab96f72c836dd1a6829e2244d520dd49
ivannz <ivannz@yandex.ru>
2020-07-17 11:50:59 +0300

    wandb agents freeze -- try dedicated tmux sessions instead of bash ampersand

--- scripts/sweeper.sh
+++ scripts/sweeper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Runs wandb agents on separate devices
+# Runs wandb agents on separate devices in detached tmux-sessions
 #
 # $ ./sweeper.sh `sweep-id` `gpu` `[* gpu]`
 #
@@ -13,8 +13,16 @@
 sweep="${1}"; shift
 
 for device in "$@"; do
-    # `ampersand` forks into a parallel bash process
-    CUDA_VISIBLE_DEVICES=${device} wandb agent "${sweep}" > /dev/null &
+    # for better random names see <https://gist.github.com/earthgecko/3089509>
+    name=$(hexdump -n 16 -v -e '/1 "%02X"' /dev/urandom)
+
+    # w/o any commands tmux starts a new session with `bash` and which resets
+    # conda env to base. Instead we just run env folllowed by python within
+    # the environment, from where tmux was launched.
+    tmux new-session -d -s "${name}" \
+        env CUDA_VISIBLE_DEVICES=${device} \
+            wandb agent "${sweep}"
+    # for debug: python -c"import torch; print(torch.cuda.device_count())"
 
     # wait for 2-7 seconds
     sleep $((2 + RANDOM % 5));
